116184ad679d3d0531761b39377466a36d58dad8
rs6571 <rs6571@rit.edu>
2018-04-24 20:44:13 -0400

    began adding move functionality

--- Simplex_17-18_Spring-master (1)/Simplex_17-18_Spring-master/00_Sandbox/Planet.cpp
+++ Simplex_17-18_Spring-master (1)/Simplex_17-18_Spring-master/00_Sandbox/Planet.cpp
@@ -19,6 +19,62 @@ bool Planet::IsColliding(Planet one, Planet two)
 		return false;
 }
 
+void Planet::ApplyForce(vector3 forceA) 
+{
+
+	force += forceA;
+
+}
+
+void Planet::CalculateAcceleration(float timeA) {
+
+	acceleration = (velocity - previousVelocity) / timeA;
+
+}
+
+void Planet::CalculateVelocity(float timeA) {
+
+	velocity = (center - previousPosition) / timeA;
+
+	velocity = velocity + acceleration * timeA;
+
+}
+
+void Planet::Move() {
+
+	vector3 newVelocity;
+
+	force = mass * acceleration; 
+
+	newVelocity = velocity + ((force / mass) * CLOCKS_PER_SEC);
+
+	center += force;
+
+	/*
+	previousPosition = center;
+
+	force = mass * acceleration;
+
+	float length = sqrt(pow(force.x, 2) + pow(force.y, 2) + pow(force.z, 2));
+	vector3 normalized = force / length;
+
+	center.x += normalized.x * velocity.x;
+	center.y += normalized.y * velocity.y;
+	center.z += normalized.z * velocity.z;
+
+	velocity.x = (center.x - previousPosition.x) / CLOCKS_PER_SEC;
+	velocity.y = (center.y - previousPosition.y) / CLOCKS_PER_SEC;
+	velocity.z = (center.z - previousPosition.z) / CLOCKS_PER_SEC;
+	*/
+
+	//float length = sqrt(pow(force.x, 2) + pow(force.y, 2) + pow(force.z, 2));
+
+	//vector3 normalized = force / length;
+
+	//position += normalized * velocity;
+
+}
+
 vector3 Planet::GetDirection(void) { return direction; }
 float Planet::GetRadius(void) { return radius; }
 vector3 Planet::GetCenter(void) { return center; }
--- Simplex_17-18_Spring-master (1)/Simplex_17-18_Spring-master/00_Sandbox/Planet.h
+++ Simplex_17-18_Spring-master (1)/Simplex_17-18_Spring-master/00_Sandbox/Planet.h
@@ -1,21 +1,43 @@
 #pragma once
 #include "Definitions.h"
+#include "Math.h"
 
 namespace Simplex
 {
 	class Planet
 	{
 		private:
+			vector3 acceleration;
+			vector3 velocity;
+			vector3 previousVelocity;
+			//vector3 position;
 			vector3 direction;
 			vector3 force;
 			vector3 center;
+			vector3 previousPosition;
+			float mass;
 			float radius;
 			String nameOfPlanet;
 
+			vector3 previousPosition;
+
+			float time;
+
 		public:
+
+			//vector3 acceleration;
+
 			Planet(vector3 p_direction, vector3 p_force, vector3 p_center, float p_radius, String name);
 
 			bool IsColliding(Planet one, Planet two);
+
+			void ApplyForce(vector3 forceA);
+
+			void CalculateAcceleration(float timeA);
+
+			void CalculateVelocity(float timeA);
+
+			void Move();
 
 			vector3 GetDirection(void);
 			float GetRadius(void);
